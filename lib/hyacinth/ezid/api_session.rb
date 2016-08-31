18a80735e72718581b63842c8943e12de405b9cf
Fred Duby <fcd1@columbia.edu>
2016-08-31 13:44:00 -0400

    Make changes required by Rubocop. All changes cosmetic.
    The Rubocop-required changes covered in this commit are all
    cosmetic (trailing white space, new hash format, identation).
    There is one change that is still pending:
    Method definitions must not be nested. Use lambda instead.

--- lib/hyacinth/ezid/api_session.rb
+++ lib/hyacinth/ezid/api_session.rb
@@ -20,18 +20,18 @@ module Ezid
     SECURESERVER = EZID[:url]
     TESTUSERNAME = EZID[:test_user]
     TESTPASSWORD = EZID[:test_password]
-    SCHEMES = { :ark => 'ark:/', :doi => 'doi:' }
+    SCHEMES = { ark: 'ark:/', doi: 'doi:' }
 
     PRIVATE = 'reserved'
     PUBLIC = 'public'
     UNAVAIL = 'unavailable'
 
-    TESTSHOULDER = { SCHEMES[:ark] => EZID[:shoulder][:ark], 
+    TESTSHOULDER = { SCHEMES[:ark] => EZID[:shoulder][:ark],
                      SCHEMES[:doi] => EZID[:shoulder][:doi] }
     TESTMETADATA = { '_target' => 'http://example.org/opensociety',
-      'erc.who' => 'Karl Popper',
-      'erc.what' => 'The Open Society and Its Enemies',
-      'erc.when' => '1945' }
+                     'erc.who' => 'Karl Popper',
+                     'erc.what' => 'The Open Society and Its Enemies',
+                     'erc.when' => '1945' }
 
     def initialize(username = TESTUSERNAME, password = TESTPASSWORD, scheme = :ark, naa = '')
       if username == TESTUSERNAME
@@ -51,7 +51,6 @@ module Ezid
     end
 
     def mint(metadata = {})
-      
       shoulder = @scheme + @naa
       metadata['_status'] = PRIVATE
       request_uri = "/shoulder/#{shoulder}"
@@ -78,7 +77,7 @@ module Ezid
 
     def build_identifier(identifier)
       unless identifier.start_with?(ApiSession::SCHEMES[:ark]) ||
-          identifier.start_with?(ApiSession::SCHEMES[:doi])
+             identifier.start_with?(ApiSession::SCHEMES[:doi])
         identifier = @scheme + @naa + identifier
       end
       identifier
@@ -101,13 +100,13 @@ module Ezid
     # public utility methods
 
     # def record_modify(identifier, metadata, clear = false)
-      # if clear
-        # # TODO: clear old metadata
-      # end
-      # metadata.each do |name, value|
-        # modify(identifier, name, value)
-      # end
-      # get(identifier)
+    #   if clear
+    #     # TODO: clear old metadata
+    #   end
+    #   metadata.each do |name, value|
+    #     modify(identifier, name, value)
+    #   end
+    #   get(identifier)
     # end
 
     def scheme=(scheme)
@@ -118,77 +117,71 @@ module Ezid
 
     private
 
-    def call_api(request_uri, request_method, request_data = nil)
-      uri = URI(SECURESERVER + request_uri)
+      def call_api(request_uri, request_method, request_data = nil)
+        uri = URI(SECURESERVER + request_uri)
 
-      # which HTTP method to use?
-      if request_method == :get
-        request = Net::HTTP::Get.new uri.request_uri
-      elsif request_method == :put
-        request = Net::HTTP::Put.new uri.request_uri
-        request.body = make_anvl(request_data)
-      elsif request_method == :post
-        request = Net::HTTP::Post.new uri.request_uri
-        request.body = make_anvl(request_data)
-        
-        #request.body = '_status: public'
-      elsif request_method == :delete
-        request = Net::HTTP::Delete.new uri.request_uri
-      end
-
-      request.basic_auth @user, @pass   
-      request.add_field('Content-Type', 'text/plain; charset=UTF-8')
-      
-      # Make the call 
-      
-      # this is commented implementation for ruby 1.9.*       
-      # result = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|     
-        # parse_record http.request(request).body
-      # end
-      
-      # this is implementation for ruby 1.8.7
-      https = Net::HTTP.new(uri.host, uri.port)
-      https.use_ssl = true
-      result = https.start do |http|     
-        
-        Rails.logger.info "=== EZID request result: " + http.request(request).body
-        parse_record http.request(request).body
-
-      end   
-      
-      Ezid::ServerResponse.new(result)
-    end
-
-    def parse_record(ezid_response)
-      parts = ezid_response.split("\n")
-      identifier = parts[0].split(': ')[1]
-      metadata = {}
-      if parts.length > 1
-        parts[1..-1].each do |p|
-          pair = p.split(': ')
-          metadata[pair[0]] = pair[1]
+        # which HTTP method to use?
+        if request_method == :get
+          request = Net::HTTP::Get.new uri.request_uri
+        elsif request_method == :put
+          request = Net::HTTP::Put.new uri.request_uri
+          request.body = make_anvl(request_data)
+        elsif request_method == :post
+          request = Net::HTTP::Post.new uri.request_uri
+          request.body = make_anvl(request_data)
+          # request.body = '_status: public'
+        elsif request_method == :delete
+          request = Net::HTTP::Delete.new uri.request_uri
         end
-        record = { 'identifier' => identifier, 'metadata' => metadata }
-      else
-        record = identifier
-      end
-      record
-    end
 
-    def make_anvl(metadata)
-      
-      def escape(s)
-        URI.escape(s, /[%:\n\r]/)
+        request.basic_auth @user, @pass
+        request.add_field('Content-Type', 'text/plain; charset=UTF-8')
+
+        # Make the call
+
+        # this is commented implementation for ruby 1.9.*
+        # result = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+        #   parse_record http.request(request).body
+        # end
+
+        # this is implementation for ruby 1.8.7
+        https = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl = true
+        result = https.start do |http|
+          Rails.logger.info "=== EZID request result: " + http.request(request).body
+          parse_record http.request(request).body
+        end
+        Ezid::ServerResponse.new(result)
       end
-      anvl = ''
-      metadata.each do |n, v|
-        anvl += escape(n.to_s) + ': ' + escape(v.to_s) + "\n"
+
+      def parse_record(ezid_response)
+        parts = ezid_response.split("\n")
+        identifier = parts[0].split(': ')[1]
+        metadata = {}
+        if parts.length > 1
+          parts[1..-1].each do |p|
+            pair = p.split(': ')
+            metadata[pair[0]] = pair[1]
+          end
+          record = { 'identifier' => identifier, 'metadata' => metadata }
+        else
+          record = identifier
+        end
+        record
       end
-      # remove last newline. there is probably a really good way to
-      # avoid adding it in the first place. if you know it, please fix.
-      #anvl.strip.encode!('UTF-8')
-       
-      anvl.strip
-    end
+
+      def make_anvl(metadata)
+        def escape(s)
+          URI.escape(s, /[%:\n\r]/)
+        end
+        anvl = ''
+        metadata.each do |n, v|
+          anvl += escape(n.to_s) + ': ' + escape(v.to_s) + "\n"
+        end
+        # remove last newline. there is probably a really good way to
+        # avoid adding it in the first place. if you know it, please fix.
+        # anvl.strip.encode!('UTF-8')
+        anvl.strip
+      end
   end
 end
